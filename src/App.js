1a15b01515a6cdae43018a5bf168dde681326c2f
Dan Lubbers <lubbers.dan@gmail.com>
2019-12-03 19:05:45 -0500

    changed redirect to webdev.danlubbers.com secure

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
         <img className='logo' src={Logo} alt='dan lubbers logo' />
           <div className='btn-container'>
             <a href='http://www.danlubbersphotographs.com' target='_blank' rel='noopener noreferrer'><button className='photoBtn' data-aos='zoom-in'>Photography / Retouching</button></a>
-            <a href='http://webdev.danlubbers.com' target='_blank' rel='noopener noreferrer'><button className='webDevBtn' data-aos='zoom-in'>Web Development</button></a>
+            <a href='https://webdev.danlubbers.com' target='_blank' rel='noopener noreferrer'><button className='webDevBtn' data-aos='zoom-in'>Web Development</button></a>
           </div>
       </section>
     );

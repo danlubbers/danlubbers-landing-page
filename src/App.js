b6c112b8060dc2f3779f5352ba07818072c3c3c4
Dan Lubbers <lubbers.dan@gmail.com>
2021-09-20 15:21:56 -0400

    Took out the www in the link for danlubbersphotographs.com as that link is unsecure and invalid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
-import './App.scss';
-import Logo from './assets/danlubbers-logo.png';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { Component } from "react";
+import "./App.scss";
+import Logo from "./assets/danlubbers-logo.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 class App extends Component {
-
   componentDidMount() {
-    AOS.init({duration: 3000});
-  } 
+    AOS.init({ duration: 3000 });
+  }
 
   render() {
-
     return (
-
       <section className="App">
-        <img className='logo' src={Logo} alt='dan lubbers logo' />
-          <div className='btn-container'>
-            <a href='http://www.danlubbersphotographs.com' target='_blank' rel='noopener noreferrer'><button className='photoBtn' data-aos='zoom-in'>Photography / Retouching</button></a>
-            <a href='https://webdev.danlubbers.com' target='_blank' rel='noopener noreferrer'><button className='webDevBtn' data-aos='zoom-in'>Web Development</button></a>
-          </div>
+        <img className="logo" src={Logo} alt="dan lubbers logo" />
+        <div className="btn-container">
+          <a
+            href="http:// danlubbersphotographs.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="photoBtn" data-aos="zoom-in">
+              Photography / Retouching
+            </button>
+          </a>
+          <a
+            href="https://webdev.danlubbers.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="webDevBtn" data-aos="zoom-in">
+              Web Development
+            </button>
+          </a>
+        </div>
       </section>
     );
   }
